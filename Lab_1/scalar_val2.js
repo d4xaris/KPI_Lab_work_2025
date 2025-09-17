00fb12446a940d0cc862016c891b7b6262093bcd
d4xaris <katerynaiakhnovska@gmail.com>
2025-09-18 00:29:24 +0300

    First Lab completed

--- Lab_1/scalar_val2.js
+++ Lab_1/scalar_val2.js
@@ -1,7 +1,7 @@
-// Скалярincні типи та покликання (2)
+// Скалярні типи та покликання (2)
 
 function inc(num) {
-    num.a = num.a + 1;
+    num.val = num.val + 1;
 }
 const obj = {a: 5};
 inc(obj);
