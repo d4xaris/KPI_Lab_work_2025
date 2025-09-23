0ab27ac74a180ef4fc9e07124bd24587eb3ac98c
d4xaris <katerynaiakhnovska@gmail.com>
2025-09-23 16:00:14 +0300

    unfinished

---
+++ Lab_2/identifiers.js
@@ -0,0 +1,5 @@
+let userName = "Katy";
+const birthDate = 2008;
+
+const greeting = () => (`Hello ${userName}, ${birthDate}`);
+console.log(greeting(userName));
---
+++ Lab_2/loops2.js
@@ -0,0 +1,19 @@
+// Цикл для непарних чисел
+
+const oddRange = (start, end) => {
+  const result = [];
+  let i = start % 2 === 0 ? start + 1 : start;
+  for (; i <= end; i += 2) {
+    result.push(i);
+  }
+  return result;
+}
+console.log(oddRange(15, 30));
+
+/* let i;
+if (start % 2 === 0) {
+  i = start + 1; 
+} else {
+  i = start; 
+} 
+*/
