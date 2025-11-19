8a96e4253d649c55cf0ebb8f7a7beebff80b6417
d4xaris <katerynaiakhnovska@gmail.com>
2025-11-19 18:26:43 +0200

    fix

--- Lab_4/forin.js
+++ Lab_4/forin.js
@@ -1,8 +1,15 @@
-function sum(...args) {
-  let total = 0;
-  for (const i in args);
-  {
+// for in 
+
+  function sum(...args){
+  let total = 0
+  for (const i in args){
     total += args[i];
   }
   return total;
 }
+
+console.log(sum(1, 2, 3));
+console.log(sum());
+console.log(sum(100, -100));
+console.log(sum(1, 6, 1000));
+console.log(sum(0));
